0a503fe3b62078d52324ac633eacb2c78e1163ee
¨PedroMiguelAlmeida¨ <pedromiguel.almd@gmail.com>
2021-11-17 12:50:42 +0000

    Conflit resolved

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,25 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <CreateVcard/>
-</template>
-
-
-<script>
-
-import CreateVcard from './components/CreateVcard.vue'
-
-export default {
-  name: 'app',
-  components: {
-    CreateVcard
-  },
-  data () {
-    return {
-    }
-  },
-  methods: {
-
-=======
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
@@ -31,32 +10,10 @@ export default {
   name: 'App',
   components: {
     HelloWorld
->>>>>>> a0c8f9e02fc6586ac3c29865e2649609e267f30b
   }
 }
 </script>
 
-<<<<<<< HEAD
-<style lang="scss">
-
-
-.avatar-img {
-  margin: -1.2rem 0.8rem -2rem 0.8rem;
-  width: 3.3rem;
-  height: 3.3rem;
-  // border-width: 1px;
-  // border-color: rgb(33, 37, 41);
-  // border-style: solid;
-}
-.avatar-text {
-  line-height: 2.2rem;
-  margin: 1rem 0.5rem -2rem 0;
-  padding-top: 1rem;
-}
-
-.dropdown-item {
-  font-size: 0.875rem;
-=======
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -65,6 +22,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
->>>>>>> a0c8f9e02fc6586ac3c29865e2649609e267f30b
 }
 </style>
